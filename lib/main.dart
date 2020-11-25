f39806eb71fd8e3887aa440768e5d011fa3516a4
Cohetero <mauriciocohetero@gmail.com>
2020-11-25 04:04:46 -0600

    SplashScreen
    Pantalla de inicio

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:schiary/screens/login_screen.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +12,23 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Login UI',
       theme: ThemeData(brightness: Brightness.dark),
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: FirstActivity(),
+      //home: LoginScreen(),
+    );
+  }
+}
+
+class FirstActivity extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+      seconds: 5,
+      navigateAfterSeconds: LoginScreen(),
+      image: Image.asset("assets/logos/Schiary.png"),
+      backgroundColor: Colors.black,
+      styleTextUnderTheLoader: new TextStyle(),
+      photoSize: 200.0,
+      loaderColor: Colors.green,
     );
   }
 }
